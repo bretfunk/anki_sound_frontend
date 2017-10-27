4602d7c49ca1cf9cab9061d8897a7340111faac8
BFunk <bret.e.funk@gmail.com>
2017-10-26 21:32:44 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+const url = "https://protected-thicket-11517.herokuapp.com/api/phrases"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {phrasesReceived: ""}
+    this.getPhrases = this.getPhrases.bind(this)
+  }
+
+  getPhrases() {
+     axios.get(url, { crossdomain: true })
+      .then((response) => {
+        alert(response)
+        this.setState({phrasesReceieved: JSON.stringify(response)})
+      })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <button
+      onClick={this.getPhrases}
+      style={{marginTop: '25vh'}}
+      >
+      Get Phrases
+      </button>
+      <p>{this.state.phrasesReceived}</p>
+      <p>Fake Text</p>
       </div>
     );
   }
